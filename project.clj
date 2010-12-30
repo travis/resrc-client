7bbabfc914edfe1dfb5505dd003c6950c2c8d662
Travis Vachon <travis.vachon@gmail.com>
2010-12-30 10:45:21 -0500

    Initial commit on resrc-client
    Moved this out of the main resrc project for clarity.

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject resrc-client "0.0.2-SNAPSHOT"
+  :description "FIXME: write"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [resrc "0.0.2-SNAPSHOT"]])
